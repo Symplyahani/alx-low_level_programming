bd27b77bee5cad430302c5bb54d966a40cfa50b3
Symplyahani <elechorm@gmail.com>
2023-04-24 14:57:58 -0700

    if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,29 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Determines if a number is positive, negative or zero.
+ * main - Prints the alphabet.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+for (i = 0; i < 52; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,30 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
- * main - Determines either greater than 5, is less than 6, or is 0
- *
- * Return: Always 0 (Success)
+ * main - prints the stdout
+ * Return: 0 if successful
  */
 int main(void)
 {
-int n, l;
+char lowerCase, not, nott;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
-if (l > 5)
+not = 'e';
+nott = 'q';
+
+for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, l);
+if (lowerCase != not && lowerCase != nott)
+putchar(lowerCase);
 }
-else if (l == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, l);
-}
-else
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
+putchar('\n');
 return (0);
 }
